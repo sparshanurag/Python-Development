f1d054cc575f20c1589bdfa522ef02798a15cde6
sparshanurag <sparshanurag@gmail.com>
2021-06-29 15:35:47 +0530

    Added files

---
+++ python/Chapter-3/02_string_slicing.py
@@ -0,0 +1,13 @@
+# greeting = "Good Morning, "
+
+# name = "Sparsh"
+# c = greeting + name
+
+# print(c)
+
+name = "Sparsh Anurag" 
+print(name[5])
+
+# name[3] = "d" --> does not work
+
+print(name[0:3])  #-> 0,1,2 [start:end:interval]
