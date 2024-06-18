a5df8ef6b1c57bae923f905de8000f972bdf1574
Sparsh Anurag <sparsh.anurag@ultragenicglobal.com>
2024-06-18 20:14:54 +0530

    Add links, limit, news and subject

--- app-5-news-api-email/main.py
+++ app-5-news-api-email/main.py
@@ -1,8 +1,15 @@
 import requests
+from send_email import send_email
 
+
+topic = "tesla"
 api_key = "921a15f6978740a5b6435509c61517ab"
 
-url = "https://newsapi.org/v2/everything?q=tesla&from=2024-05-18&sortBy=publishedAt&apiKey=921a15f6978740a5b6435509c61517ab"
+url = "https://newsapi.org/v2/everything?" \
+f"q={topic}&" \
+"sortBy=publishedAt&" \
+"apiKey=921a15f6978740a5b6435509c61517ab&" \
+"language=en"
 
 #Make request
 request = requests.get(url)
@@ -11,5 +18,28 @@ request = requests.get(url)
 content = request.json()
 
 # Access the article titles and description
-for article in content["articles"]:
-    print(article["title"])
+
+body = ""
+
+# Initialize the 'body' variable
+body = ""
+
+# Loop through each article in the content
+for article in content["articles"][:20]:
+    # Ensure title, description, and url are strings
+    title = article["title"] if article["title"] is not None else ""
+    description = article["description"] if article["description"] is not None else ""
+    url = article["url"] if article["url"] is not None else ""
+    
+    # Check if the title is not an empty string
+    if title:
+        # Concatenate the title, description, and URL to the body string
+        body += "Subject: Today's news" + title + "\n" + description + "\n" + url + "\n\n"
+
+print(body)
+
+
+
+body = body.encode("utf-8")
+
+send_email(message=body)
